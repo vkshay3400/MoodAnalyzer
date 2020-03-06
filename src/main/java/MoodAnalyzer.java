b18cf2d8a09df6f73291f470b5de58cd8eb7e646
vkshay3400 <akshayp2208@gmail.com>
2020-03-06 18:52:33 +0530

    Added: Validate mood sad or happy

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,2 +1,19 @@
 public class MoodAnalyzer {
+    String mood;
+
+    //DECLARE MAIN METHOD
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyzer");
+    }
+
+    //METHOD TO VALIDATE MOOD
+    public String moodAnalyzer(String recentMood){
+        if (recentMood.equals("I am in sad mood")){
+            mood="Sad";
+        }
+        else if (recentMood.equals("I am in any mood")){
+            mood="Happy";
+        }
+        return mood;
+    }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,2 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+    MoodAnalyzer mood=new MoodAnalyzer();
+
+    //TEST CASE TO CHECK VALID MOOD
+    @Test
+    public void givenMessage_whenValid_thenReturn() {
+        String recentmood="I am in sad mood";
+        String reaction=mood.moodAnalyzer(recentmood);
+        Assert.assertEquals("Sad", reaction);
+    }
+
+    //TEST CASE TO CHECK INVALID MOOD
+    @Test
+    public void givenMessage_whenInvalid_thenReturn() {
+        String recentmood="I am in any mood";
+        String reaction=mood.moodAnalyzer(recentmood);
+        Assert.assertEquals("Happy", reaction);
+    }
 }
